2ddeb853c53dfbbec9de52dd89fe656032ecf94f
akash-borate <27akashborate@gmail.com>
2025-04-23 11:33:33 +0530

    basics

---
+++ array-basic.js
@@ -0,0 +1,13 @@
+// Array - Declaration
+let arr = ["apple", "banana", "cherry"];
+console.log(arr);
+let details = {
+    age: 20,
+    address: "Pune",
+}
+let arr2 = ["akash", details, 27, true, null, undefined];
+arr2.push("new");
+arr2.pop();
+arr2.unshift("new");
+arr2.shift();
+console.log(arr2);
